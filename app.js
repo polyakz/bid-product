9b2b6f933a23f373135f3814cd8c525f4e7b9396
simi <simonka.janos93@gmail.com>
2019-11-16 16:24:01 +0100

    Templates, assets, second page added

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const port = 3000;
 
 app.use(express.static('assets'));
 app.get('/', (req, res) => res.sendFile('views/index.html' , { root : __dirname}));
+app.get('/bid/1', (req, res) => res.sendFile('views/single-1.html' , { root : __dirname}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
