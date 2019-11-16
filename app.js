c9c15267294763e79632463e8ca9d4ebace64585
simi <simonka.janos93@gmail.com>
2019-11-16 14:59:08 +0100

    Base template, assets added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('assets'));
+app.get('/', (req, res) => res.sendFile('views/index.html' , { root : __dirname}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
